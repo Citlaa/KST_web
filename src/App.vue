3e8caf4b797ec71e1bfee021a09b39408708d489
Citlalli Jara Díaz de León <citla.jadi@gmail.com>
2021-08-30 20:23:43 -0500

    Se termina menú

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,35 @@
 <template>
   <div>
-    <b-navbar toggleable="md" type="dark" variant="danger" style="padding-left: 20px; padding-right: 20px;">      
-      <b-navbar-brand href="#">NavBar</b-navbar-brand>
+    <div>
+    <b-navbar toggleable="md" type="dark" style="padding-left: 20px; padding-right: 20px;">      
+      <b-navbar-brand href="#">
+        <img :src="logo.url" class="d-inline-block align-top logo" alt="Instituto Castelazo">
+      </b-navbar-brand>
       <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
-      <b-collapse id="nav-collapse" is-nav>
+      <b-collapse id="nav-collapse" class="justify-content-end" is-nav>
         <b-navbar-nav>
-          <b-nav-item :to="{ name: 'Index'}">Home</b-nav-item>
-          <b-nav-item :to="{ name: 'Home'}">Disabled</b-nav-item>
-        </b-navbar-nav>
+          <b-nav-item :to="{ name: 'Index'}">Inicio</b-nav-item>
+          <b-nav-item-dropdown text="Administración">
+            <b-dropdown-item :to="{ name: 'TiposDePago'}">Tipos de pago</b-dropdown-item>          
+            <b-dropdown-item :to="{ name: 'TiposDePago'}">Creación de grupos</b-dropdown-item>
+            <b-dropdown-item :to="{ name: 'TiposDePago'}">Ciclo Escolar</b-dropdown-item>
+            <b-dropdown-item :to="{ name: 'TiposDePago'}">Control de pagos</b-dropdown-item>            
+            <b-dropdown-item :to="{ name: 'TiposDePago'}">Tipos de recargos</b-dropdown-item>            
+          </b-nav-item-dropdown>
+          <b-nav-item-dropdown text="Alumnos">
+            <b-dropdown-item :to="{ name: 'TiposDePago'}">Registro de alumnos</b-dropdown-item>          
+            <b-dropdown-item :to="{ name: 'TiposDePago'}">Consulta de Historial Acádemico</b-dropdown-item>            
+            <b-dropdown-item :to="{ name: 'TiposDePago'}">Rgistro de Padre/Tutor</b-dropdown-item>            
+          </b-nav-item-dropdown>          
+          <b-nav-item-dropdown text="Reportes">
+            <b-dropdown-item :to="{ name: 'TiposDePago'}" class="ms-auto">Lista de grupo</b-dropdown-item>          
+            <b-dropdown-item :to="{ name: 'TiposDePago'}">Lista de personas con aduedo</b-dropdown-item>                        
+          </b-nav-item-dropdown>
+        </b-navbar-nav>        
       </b-collapse>      
     </b-navbar>
+    </div>
     <div id="app" class="container is-max-desktop">
       <router-view />
     </div>
@@ -25,6 +44,13 @@ export default {
     
   },
   setup(){    
+  },
+  data(){
+    return {
+      logo: {
+        url: require('./assets/logo.png')
+      }
+    }
   }
 };
 </script>
@@ -32,4 +58,27 @@ export default {
 <style>
 /* import style bulma */
 @import "~bulma/css/bulma.css";
+
+.navbar{
+  background-color: #002d5c !important;  
+}
+.router-link-active{
+  color:blue;
+
+}
+
+.navbar-dark .navbar-nav .nav-link {
+    color: #FFFFFF !important;
+}
+
+.dropdown-menu {    
+    right: 0;
+    left: auto;
+}
+
+.logo{
+  position: absolute;
+  top: 0px;
+  max-width: 200px;
+}
 </style>
